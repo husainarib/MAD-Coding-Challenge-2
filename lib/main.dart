0344c238a5dd0824e39435706d590bfe721711e9
Arib Husain <husainarib@gmail.com>
2024-10-07 18:37:36 -0400

    add more images

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,18 @@ class HalloweenApp extends StatefulWidget {
 }
 
 class _HalloweenGameState extends State<HalloweenApp> {
-  late double _batTop, _batLeft;
-  String img = "lib/img/bat.png";
+  late double _batTop,
+      _batLeft,
+      _pkTop,
+      _pkLeft,
+      _ghostTop,
+      _ghostLeft,
+      _frTop,
+      _frLeft;
+  String bat = "lib/img/bat.png";
+  String pk = "lib/img/pumpkin.png";
+  String ghost = "lib/img/ghost.png";
+  String fred = "lib/img/freddy.png";
   late AudioPlayer _audioPlayer;
 
   @override
@@ -21,6 +31,9 @@ class _HalloweenGameState extends State<HalloweenApp> {
     _initializePosition();
     Timer.periodic(Duration(seconds: 2), (timer) {
       _moveBat();
+      _movePumpkin();
+      _moveGhost();
+      _moveFred();
     });
     _audioPlayer = AudioPlayer();
     _playBackgroundMusic();
@@ -39,13 +52,43 @@ class _HalloweenGameState extends State<HalloweenApp> {
   void _initializePosition() {
     _batTop = 100;
     _batLeft = 50;
+    _pkTop = 200;
+    _pkLeft = 100;
+    _ghostTop = 300;
+    _ghostLeft = 150;
+    _frTop = 400;
+    _frLeft = 200;
   }
 
   void _moveBat() {
     setState(() {
       Random random = Random();
-      _batTop = random.nextDouble() * 600;
-      _batLeft = random.nextDouble() * 1000;
+      _batTop = random.nextDouble() * 700;
+      _batLeft = random.nextDouble() * 1500;
+    });
+  }
+
+  void _movePumpkin() {
+    setState(() {
+      Random random = Random();
+      _pkTop = random.nextDouble() * 700;
+      _pkLeft = random.nextDouble() * 1500;
+    });
+  }
+
+  void _moveGhost() {
+    setState(() {
+      Random random = Random();
+      _ghostTop = random.nextDouble() * 700;
+      _ghostLeft = random.nextDouble() * 1500;
+    });
+  }
+
+  void _moveFred() {
+    setState(() {
+      Random random = Random();
+      _frTop = random.nextDouble() * 700;
+      _frLeft = random.nextDouble() * 1500;
     });
   }
 
@@ -64,7 +107,27 @@ class _HalloweenGameState extends State<HalloweenApp> {
             top: _batTop,
             left: _batLeft,
             duration: const Duration(seconds: 2),
-            child: Image.asset(img, width: 80),
+            child: Image.asset(bat, width: 80),
+          ),
+          AnimatedPositioned(
+            top: _pkTop,
+            left: _pkLeft,
+            duration: const Duration(seconds: 2),
+            child: Image.asset(pk, width: 80),
+          ),
+          //ghost
+          AnimatedPositioned(
+            top: _ghostTop,
+            left: _ghostLeft,
+            duration: const Duration(seconds: 2),
+            child: Image.asset(ghost, width: 80),
+          ),
+          //freddy
+          AnimatedPositioned(
+            top: _frTop,
+            left: _frLeft,
+            duration: const Duration(seconds: 2),
+            child: Image.asset(fred, width: 80),
           ),
         ],
       ),
